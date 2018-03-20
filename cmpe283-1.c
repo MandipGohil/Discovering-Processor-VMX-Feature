cb1454bc68a22c59c3867a2f088e5a99757a6ec2
Mandipsinh Gohil <mandipsinh046@gmail.com>
2018-03-19 17:15:04 -0700

    C File update

--- cmpe283-1.c
+++ cmpe283-1.c
@@ -1,8 +1,8 @@
-password/*
+/*
  *  cmpe283-1.c - Kernel module for CMPE283 assignment 1
  */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/module.h>   /* Needed by all modules */
+#include <linux/kernel.h>   /* Needed for KERN_INFO */
 #include <asm/msr.h>
 
 #define MAX_MSG 80
@@ -11,10 +11,16 @@ password/*
  * Model specific registers (MSRs) by the module.
  * See SDM volume 4, section 2.1
  */
-#define IA32_VMX_PINBASED_CTLS 0x481
-#define IA32_VMX_BASIC 0x480
-
-
+#define IA32_VMX_PINBASED_CTLS  0x481
+#define IA32_VMX_BASIC  0x480
+#define IA32_VMX_PROCBASED_CTLS 0x482
+#define IA32_VMX_PROCBASED_CTLS2 0x48B
+#define IA32_VMX_EXIT_CTLS 0x483
+#define IA32_VMX_ENTRY_CTLS 0x484
+#define IA32_VMX_TRUE_PINBASED_CTLS 0x48D
+#define IA32_VMX_TRUE_PROCBASED_CTLS 0x48E
+#define IA32_VMX_TRUE_EXIT_CTLS 0x48F
+#define IA32_TRUE_ENTRY_CTLS 0x490
 /*
  * struct caapability_info
  *
@@ -22,8 +28,8 @@ password/*
  * Used by report_capability to output VMX capabilities.
  */
 struct capability_info {
-	uint8_t bit;
-	const char *name;
+    uint8_t bit;
+    const char *name;
 };
 
 
@@ -33,14 +39,14 @@ struct capability_info {
  */
 struct capability_info pinbased[5] =
 {
-	{ 0, "External Interrupt Exiting" },
-	{ 3, "NMI Exiting" },
-	{ 5, "Virtual NMIs" },
-	{ 6, "Activate VMX Preemption Timer" },
-	{ 7, "Process Posted Interrupts" }
+    { 0, "External Interrupt Exiting" },
+    { 3, "NMI Exiting" },
+    { 5, "Virtual NMIs" },
+    { 6, "Activate VMX Preemption Timer" },
+    { 7, "Process Posted Interrupts" }
 };
 
-struct capability_info basic[5] =
+struct capability_info truepinbased[5] =
 {
     { 0, "External Interrupt Exiting" },
     { 3, "NMI Exiting" },
@@ -48,6 +54,145 @@ struct capability_info basic[5] =
     { 6, "Activate VMX Preemption Timer" },
     { 7, "Process Posted Interrupts" }
 };
+
+struct capability_info basic[5] =
+{
+    { 55, "True controls capability" }
+};
+
+struct capability_info procbased[21] =
+{
+    { 2, "Interrupt-window exiting" },
+    { 3, "Use TSC offsetting" },
+    { 7, "HLT exiting" },
+    { 9, "INVLPG exiting" },
+    { 10, "MWAIT exiting" },
+    { 11, "RDPMC exiting" },
+    { 12, "RDTSC exiting" },
+    { 15, "CR3-load exiting" },
+    { 16, "CR3-store exiting" },
+    { 19, "CR8-load exiting" },
+    { 20, "CR8-store exiting" },
+    { 21, "Use TPR shadow" },
+    { 22, "NMI-window exiting" },
+    { 23, "MOV-DR exiting" },
+    { 24, "Unconditional I/O exiting" },
+    { 25, "Use I/O bitmaps" },
+    { 27, "Monitor trap flag" },
+    { 28, "Use MSR bitmaps" },
+    { 29, "MONITOR exiting" },
+    { 30, "PAUSE exiting" },
+    { 31, "Activate secondary controls" }
+};
+
+struct capability_info trueprocbased[21] =
+{
+    { 2, "Interrupt-window exiting" },
+    { 3, "Use TSC offsetting" },
+    { 7, "HLT exiting" },
+    { 9, "INVLPG exiting" },
+    { 10, "MWAIT exiting" },
+    { 11, "RDPMC exiting" },
+    { 12, "RDTSC exiting" },
+    { 15, "CR3-load exiting" },
+    { 16, "CR3-store exiting" },
+    { 19, "CR8-load exiting" },
+    { 20, "CR8-store exiting" },
+    { 21, "Use TPR shadow" },
+    { 22, "NMI-window exiting" },
+    { 23, "MOV-DR exiting" },
+    { 24, "Unconditional I/O exiting" },
+    { 25, "Use I/O bitmaps" },
+    { 27, "Monitor trap flag" },
+    { 28, "Use MSR bitmaps" },
+    { 29, "MONITOR exiting" },
+    { 30, "PAUSE exiting" },
+    { 31, "Activate secondary controls" }
+};
+
+struct capability_info procbased2[22] =
+{
+    { 0, "Virtualize APICaccesses" },
+    { 1, "Enable EPT" },
+    { 2, "Descriptor-table exiting" },
+    { 3, "Enable RDTSCP" },
+    { 4, "Virtualize x2APIC mode" },
+    { 5, "Enable VPID" },
+    { 6, "WBINVD exiting" },
+    { 7, "Unrestricted guest" },
+    { 8, "APIC-register virtualization" },
+    { 9, "Virtual-interrupt delivery" },
+    { 10, "PAUSE-loop exiting" },
+    { 11, "RDRAND exiting" },
+    { 12, "Enable INVPCID" },
+    { 13, "Enable VM functions" },
+    { 14, "VMCS shadowing" },
+    { 15, "Enable ENCLS exiting" },
+    { 16, "RDSEED exiting" },
+    { 17, "Enable PML" },
+    { 18, "EPT-violation #VE" },
+    { 19, "Conceal VMX from PT" },
+    { 20, "Enable XSAVES/XRSTORS" },
+    { 22, "Mode-based execute control for EPT" },
+    { 25, "Use TSC scaling" }
+};
+
+struct capability_info exitcontrol[11] =
+{
+    { 2, "Save debug controls" },
+    { 9, "Host addressspace size" },
+    { 12, "Load IA32_PERF_GLOB AL_CTRL" },
+    { 15, "Acknowledge interrupt on exit" },
+    { 18, "Save IA32_PAT" },
+    { 19, "Load IA32_PAT" },
+    { 20, "Save IA32_EFER" },
+    { 21, "Load IA32_EFER" },
+    { 22, "Save VMXpreemption timer value" },
+    { 23, "Clear IA32_BNDCFGS" },
+    { 24, "Conceal VMX from PT" }
+};
+
+struct capability_info trueexitcontrol[11] =
+{
+    { 2, "Save debug controls" },
+    { 9, "Host addressspace size" },
+    { 12, "Load IA32_PERF_GLOB AL_CTRL" },
+    { 15, "Acknowledge interrupt on exit" },
+    { 18, "Save IA32_PAT" },
+    { 19, "Load IA32_PAT" },
+    { 20, "Save IA32_EFER" },
+    { 21, "Load IA32_EFER" },
+    { 22, "Save VMXpreemption timer value" },
+    { 23, "Clear IA32_BNDCFGS" },
+    { 24, "Conceal VMX from PT" }
+};
+
+struct capability_info entrycontrol[9] =
+{
+    { 2, "Load debug controls" },
+    { 9, "IA-32e mode guest" },
+    { 10, "Entry to SMM" },
+    { 11, "Deactivate dualmonitor treatment" },
+    { 13, "Load IA32_PERF_GLOBA L_CTRL" },
+    { 14, "Load IA32_PAT" },
+    { 15, "Load IA32_EFER" },
+    { 16, "Load IA32_BNDCFGS" },
+    { 17, "Conceal VMX from PT" }
+};
+
+struct capability_info trueentrycontrol[9] =
+{
+    { 2, "Load debug controls" },
+    { 9, "IA-32e mode guest" },
+    { 10, "Entry to SMM" },
+    { 11, "Deactivate dualmonitor treatment" },
+    { 13, "Load IA32_PERF_GLOBA L_CTRL" },
+    { 14, "Load IA32_PAT" },
+    { 15, "Load IA32_EFER" },
+    { 16, "Load IA32_BNDCFGS" },
+    { 17, "Conceal VMX from PT" }
+};
+
 
 /*
  * report_capability
@@ -65,20 +210,20 @@ void
 report_capability(struct capability_info *cap, uint8_t len, uint32_t lo,
     uint32_t hi)
 {
-	uint8_t i;
-	struct capability_info *c;
-	char msg[MAX_MSG];
+    uint8_t i;
+    struct capability_info *c;
+    char msg[MAX_MSG];
 
-	memset(msg, 0, sizeof(msg));
+    memset(msg, 0, sizeof(msg));
 
-	for (i = 0; i < len; i++) {
-		c = &cap[i];
-		snprintf(msg, 79, "  %s: Can set=%s, Can clear=%s\n",
-		    c->name,
-		    (hi & (1 << c->bit)) ? "Yes" : "No",
-		    !(lo & (1 << c->bit)) ? "Yes" : "No");
-		printk(msg);
-	}
+    for (i = 0; i < len; i++) {
+        c = &cap[i];
+        snprintf(msg, 79, "  %s: Can set=%s, Can clear=%s\n",
+            c->name,
+            (hi & (1 << c->bit)) ? "Yes" : "No",
+            !(lo & (1 << c->bit)) ? "Yes" : "No");
+        printk(msg);
+    }
 }
 
 /*
@@ -89,19 +234,67 @@ report_capability(struct capability_info *cap, uint8_t len, uint32_t lo,
 void
 detect_vmx_features(void)
 {
-	uint32_t lo, hi;
+    uint32_t lo, hi;
 
-	/* Pinbased controls */
-	rdmsr(IA32_VMX_PINBASED_CTLS, lo, hi);
-	pr_info("Pinbased Controls MSR: 0x%llx\n",
-		(uint64_t)(lo | (uint64_t)hi << 32));
-	report_capability(pinbased, 5, lo, hi);
+    /* Pinbased controls */
+    rdmsr(IA32_VMX_PINBASED_CTLS, lo, hi);
+    pr_info("\n----->Pinbased Controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(pinbased, 5, lo, hi);
 
+    /* True Pinbased controls */
+    rdmsr(IA32_VMX_TRUE_PINBASED_CTLS, lo, hi);
+    pr_info("\n----->True Pinbased Controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(truepinbased, 5, lo, hi);
 
+    /* Basic controls */
     rdmsr(IA32_VMX_BASIC, lo, hi);
-    pr_info("Pinbased Controls MSR: 0x%llx\n",
-        (uint64_t)(lo | (uint64_t)hi << 55));
-    report_capability(basic, 5, lo, hi);
+    pr_info("\n----->Basic MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(basic, 1, lo, hi);
+
+    /* Procbased controls */
+    rdmsr(IA32_VMX_PROCBASED_CTLS, lo, hi);
+    pr_info("\n----->Procbased MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(procbased, 21, lo, hi);
+
+    /* True Procbased controls */
+    rdmsr(IA32_VMX_TRUE_PROCBASED_CTLS, lo, hi);
+    pr_info("\n----->True Procbased MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(trueprocbased, 21, lo, hi);
+
+    /* Secondary Procbased controls */
+    rdmsr(IA32_VMX_PROCBASED_CTLS2, lo, hi);
+    pr_info("\n----->Secondary Procbased MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(procbased2, 22, lo, hi);
+
+    /* Exit controls */
+    rdmsr(IA32_VMX_EXIT_CTLS, lo, hi);
+    pr_info("\n----->Exit controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(exitcontrol, 11, lo, hi);
+
+    /* True Exit controls */
+    rdmsr(IA32_VMX_TRUE_EXIT_CTLS, lo, hi);
+    pr_info("\n----->True Exit controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(trueexitcontrol, 11, lo, hi);
+
+    /* Entry controls */
+    rdmsr(IA32_VMX_ENTRY_CTLS, lo, hi);
+    pr_info("\n----->Entry controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(entrycontrol, 11, lo, hi);
+
+    /* True Control controls */
+    rdmsr(IA32_TRUE_ENTRY_CTLS, lo, hi);
+    pr_info("\n----->True Entry controls MSR: 0x%llx\n",
+        (uint64_t)(lo | (uint64_t)hi << 32));
+    report_capability(trueentrycontrol, 11, lo, hi);
 }
 
 /*
@@ -115,14 +308,14 @@ detect_vmx_features(void)
 int
 init_module(void)
 {
-	printk(KERN_INFO "CMPE 283 Assignment 1 Module Start\n");
+    printk(KERN_INFO "CMPE 283 Assignment 1 Module Start\n");
 
-	detect_vmx_features();
+    detect_vmx_features();
 
-	/*
-	 * A non 0 return means init_module failed; module can't be loaded.
-	 */
-	return 0;
+    /*
+     * A non 0 return means init_module failed; module can't be loaded.
+     */
+    return 0;
 }
 
 /*
@@ -133,5 +326,5 @@ init_module(void)
 void
 cleanup_module(void)
 {
-	printk(KERN_INFO "CMPE 283 Assignment 1 Module Exits\n");
+    printk(KERN_INFO "CMPE 283 Assignment 1 Module Exits\n");
 }
